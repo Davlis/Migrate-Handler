fe26894f70373d2c3f1bb1f8cde90877168cb9df
dliszka <ddewid@gmail.com>
2018-01-12 02:08:06 +0100

    Added nodemon to devDepedencies. Added watch script.

---
+++ dist/models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function name() {
+  console.log(_mongoose2.default);
+}
+
+name();
